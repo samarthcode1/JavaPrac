0644335300ab7f19a863d1d77d0975b205de6cb5
sai-sreekhar <sai.sreekhar@gmail.com>
2022-08-04 15:18:54 +0530

    Exno2 2 qsns added

---
+++ Exno2/pascalTriangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class pascalTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+
+        for (int i = 0; i < height; i++) {
+            StringBuilder space = new StringBuilder();
+            for (int j = 0; j < height - i; j++) {
+                space.append(" ");
+            }
+            System.out.print(space);
+            for (int k = 0; k < i + 1; k++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.print(space);
+            System.out.print('\n');
+        }
+    }
+}
+/*
+       
+
+
+
+
+
+ */
